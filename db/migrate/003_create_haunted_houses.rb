ab6b7cf7b8247d6b4b8519449bbb5df5d14880c3
Chance Smith <chancesmith97@hotmail.com>
2019-08-01 20:24:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme 
       t.integer :price 
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
       
   end 
   
