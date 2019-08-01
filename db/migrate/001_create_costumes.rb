cf8e230a31a5fda64ea2fb00b2c8c98667c368f2
Chance Smith <chancesmith97@hotmail.com>
2019-08-01 20:05:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,10 @@ class CreateCostumes < ActiveRecord::Migration
     create_table :costumes do |t|
       t.string :name 
       t.integer :price 
-      t.
+      t.string :image_url
+      t.string :size 
+      t.timestamps
+    end
   end
   
 end
